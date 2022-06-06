55236a0482d5b8cefb686e3960caadaa8a998952
asamarsal <ASAMARSAL@gmail.com>
2022-06-07 00:50:49 +0700

    Drawer

---
+++ app/src/main/java/com/example/mayafif/laporanpenjualan.java
@@ -0,0 +1,14 @@
+package com.example.mayafif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class laporanpenjualan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laporanpenjualan);
+    }
+}
