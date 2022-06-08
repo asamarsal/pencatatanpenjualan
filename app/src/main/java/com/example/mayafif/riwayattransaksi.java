b7e1f67c922c52aa4e960b4f1b244056073c0f0d
asamarsal <ASAMARSAL@gmail.com>
2022-06-08 14:38:45 +0700

    Add some feature layout

---
+++ app/src/main/java/com/example/mayafif/riwayattransaksi.java
@@ -0,0 +1,14 @@
+package com.example.mayafif;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class riwayattransaksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riwayattransaksi);
+    }
+}
